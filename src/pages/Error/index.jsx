1b647be6c974b56b56e2f954f65499b189c132c9
Francisco Molina <fdm.molina@gmail.com>
2022-06-28 19:08:48 -0300

    Add pages folder and routes

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h2>Error</h2>
+      <h5>No se encontro la pagina solicitada.</h5>
+      <button>Volver</button>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "../../components/Dashboard/";
+import "../../styles/Home.css";
+const Home = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Home;
