d08d59b23489f21338036ff7507cb04db554d93e
Francisco Molina <fdm.molina@gmail.com>
2022-07-01 00:31:08 -0300

    Add all cards container final

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Card } from "../Card/Card";
 import allData from "../../JSON/data.json";
+import "../../styles/CardContainer.css";
 
 const CardContainer = () => {
   const dataCard = allData;
 
   return (
-    <div>
+    <div className="allCard-container">
       {dataCard.map((ele) => {
         console.log(ele);
         return <Card info={ele} key={ele.title} />;
