515777a25e044e73f3e5ca4c8008b9c6d1dc63c5
Francisco Molina <fdm.molina@gmail.com>
2022-07-14 18:42:32 -0300

    Homepage ready to deploy

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
 import Dashboard from "../../components/Dashboard/";
+import Footer from "../../components/Footer/Footer";
 import "../../styles/Home.css";
 const Home = () => {
   return (
     <div className="home">
       <Dashboard />
+      <Footer />
     </div>
   );
 };
