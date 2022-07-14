27dee194f4309ea12791cb908588ad16f30a24cc
Francisco Molina <fdm.molina@gmail.com>
2022-07-14 18:41:34 -0300

    Add the footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="FooterContainer">
+      <footer className="Footer">
+        <span className="Span">
+          © Francisco Molina | Challenge of Frontendmentor.io
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
