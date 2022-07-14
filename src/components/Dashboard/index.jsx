a26b92845f3b3791cd25ae66f0694cda3d162193
Francisco Molina <fdm.molina@gmail.com>
2022-07-14 18:38:22 -0300

    Arrange the components so that the states can only be accessed with react

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import "../../styles/Dashboard.css";
-import ProfileContainer from "../ProfileContainer/ProfileContainer";
-import CardContainer from "../../components/CardContainer/CardContainer";
+import "../../styles/ProfileContaier.css";
+import "../../styles/CardContainer.css";
+import { Card } from "../Card/Card";
+import Photo from "../Photo/Photo";
+import jsonData from "../../JSON/data.json";
+
 const Dashboard = () => {
+  const dataCard = jsonData;
+  const [timeframe, setTimeframe] = useState("daily");
   return (
     <div className="dashboard">
-      <ProfileContainer />
-      <CardContainer />
+      <aside className="profile-container">
+        <div className="profile-hero">
+          <Photo />
+          <div className="profile-hero-text">
+            <h4>Report of</h4>
+            <h2>Jeremy Robson</h2>
+          </div>
+        </div>
+        <div className="profile-options">
+          <ul>
+            <li onClick={() => setTimeframe("daily")}>Daily</li>
+            <li onClick={() => setTimeframe("weekly")}>Weekly</li>
+            <li onClick={() => setTimeframe("monthly")}>Monthly</li>
+          </ul>
+        </div>
+      </aside>
+      <div className="allCard-container">
+        {dataCard.map((ele) => {
+          return <Card info={ele} key={ele.title} timeframe={timeframe} />;
+        })}
+      </div>
     </div>
   );
 };
