70083ba7ca0a96d186aab362a6ad21d0f32648fb
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-01 13:25:05 -0300

    fix styles for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,11 @@ import "../../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <div className="FooterContainer">
-      <footer className="Footer">
-        <span className="Span">
-          © Francisco Molina | Challenge of Frontendmentor.io
-        </span>
-      </footer>
-    </div>
+    <footer className="Footer">
+      <span className="Span">
+        © Francisco Molina | Challenge of Frontendmentor.io
+      </span>
+    </footer>
   );
 };
 
